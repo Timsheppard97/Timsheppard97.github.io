8685c03fbb6d2f2e9846cfb4d6779d6a2df2fd58
Timothy <timsheppard97@yahoo.com>
2022-03-07 09:55:00 -0500

    Trimmed files, touched up some areas

--- flask_app/controller/routes.py
+++
@@ -1,6 +0,0 @@
-from flask_app import app
-from flask import render_template, redirect
-
-@app.route("/")
-def homepage():
-    return render_template("homepage.html")
--- server.py
+++
@@ -1,7 +0,0 @@
-from flask_app import app
-from flask_app.controller import routes
-
-#If anymore controller files are added then they must be imported above here.
-
-if __name__ == "__main__":
-    app.run(debug=True)
